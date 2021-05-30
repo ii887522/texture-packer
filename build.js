efb040073550de2affb42adeaac2c3170c9b315a
Chi Min Yong <chimin.yong@g2g.com>
2021-05-30 18:15:18 +0800

    Migrate to github

--- build.js
+++ build.js
@@ -5,15 +5,14 @@ import { options, dependencies, zip, dll } from '@ii887522/packify'
 options.outDirPath = 'libs/'
 options.x86DllOutDirPaths = ['texture-packer/Debug/', 'texture-packer/Release/', 'texture-packer/Test/'],
 options.x64DllOutDirPaths = ['texture-packer/x64/Debug/', 'texture-packer/x64/Release/', 'texture-packer/x64/Test/']
-const accessToken = '<access-token>'
 
 dependencies(async () => {
   await Promise.all([
     zip('https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip'),
     zip('https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip'),
     zip('https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip'),
-    zip('https://gitlab.com/api/v4/projects/23530641/packages/generic/nitro/1.3.0/nitro-1.3.0.zip', { 'PRIVATE-TOKEN': accessToken }),
-    zip('https://gitlab.com/api/v4/projects/23074770/packages/generic/viewify/1.5.0/viewify-1.5.0.zip', { 'PRIVATE-TOKEN': accessToken })
+    zip('https://github.com/ii887522/nitro/releases/download/v1.3.0/nitro-1.3.0.zip'),
+    zip('https://github.com/ii887522/viewify/releases/download/v1.6.0/viewify-1.6.0.zip')
   ])
   dll('x86', 'SDL2-2.0.12/lib/x86/SDL2.dll')
   dll('x64', 'SDL2-2.0.12/lib/X64/SDL2.dll')
