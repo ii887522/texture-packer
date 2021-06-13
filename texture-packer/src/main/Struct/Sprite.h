1b5be11813814e010e676ddb473f2bc7d0a942b6
Chi Min Yong <chimin.yong@g2g.com>
2021-06-14 07:57:28 +0800

    Fix include errors for dependent projects

--- texture-packer/src/main/Struct/Sprite.h
+++ texture-packer/src/main/Struct/Sprite.h
@@ -3,7 +3,7 @@
 #ifndef TEXTURE_PACKER_SRC_MAIN_STRUCT_SPRITE_H_
 #define TEXTURE_PACKER_SRC_MAIN_STRUCT_SPRITE_H_
 
-#include <viewify/viewify.h>
+#include <viewify/Struct/Rect.h>
 
 using ii887522::viewify::Rect;
 
